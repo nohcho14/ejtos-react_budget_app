9436402fb541f2be778cbf18fd2f6822e633150f
nohcho14 <nohcho14@hotmail.com>
2023-06-03 17:12:09 -0400

    React final project

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -59,12 +59,13 @@ const AllocationForm = (props) => {
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
 
+                    <label style={{ marginLeft: '10px' }}>£</label>
                     <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
+                        style={{ size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
 
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,40 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
-    );
+  const { budget, setBudget } = useContext(AppContext);
+  const [inputValue, setInputValue] = useState(budget);
+  const limit = 20000;
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleInputBlur = () => {
+    const newBudget = parseInt(inputValue);
+    if (!isNaN(newBudget)) {
+      if (newBudget > limit) {
+        alert('The value cannot exceed remaining funds  £');
+      } else {
+        setBudget(newBudget);
+      }
+    } else {
+      setInputValue(budget);
+    }
+  };
+
+  return (
+    <div className="alert alert-secondary">
+      <span>Budget: £</span>
+      <input
+        type="number"
+        value={inputValue}
+        onChange={handleInputChange}
+        onBlur={handleInputBlur}
+        step={10}
+      />
+    </div>
+  );
 };
+
 export default Budget;
